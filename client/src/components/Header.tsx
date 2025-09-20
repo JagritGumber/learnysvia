5f48c7ec4987a0eb01485c8067eba10f044f065c
JagritGumber <gumberjagrit@gmail.com>
2025-09-20 20:53:10 +0530

    refactor(header): simplify navigation by commenting out menu and updating hamburger icon

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -17,49 +17,26 @@ export default function Header() {
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
+        {/* <div className="dropdown">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle lg:hidden"
+          >
+            <Icon icon="lineicons:menu-hamburger-1" className="size-6" />
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 border-base-300 border shadow"
           >
             <li>
               <Link to="/">Home</Link>
             </li>
           </ul>
-        </div>
+        </div> */}
         <Link to="/" className="btn btn-ghost text-xl">
           Rewiredu
         </Link>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          {isAuthenticated && (
-            <>
-              <li>
-                <Link to="/rooms">Rooms</Link>
-              </li>
-              <li>
-                <Link to="/catalog">Catalog</Link>
-              </li>
-            </>
-          )}
-        </ul>
       </div>
       <div className="navbar-end">
         {isAuthenticated && user ? (
